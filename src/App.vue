6332ab2e1ebeedfb80ef70f9761db2f15659e048
Renzo Cabarios <renzo.cabarios@gmail.com>
2022-08-29 16:38:33 +0800

    Add NavBar in component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
+import NavBar from './components/NavBar.vue';
 </script>
 
 <template>
+        <NavBar/>
 </template>
 
 <style scoped>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+</script>
+
+<template>
+        <div class="container bg-gray-900">
+            <div class="flex flex-col justify-evenly items-center px-3 py-1">
+                <div class="h-1 w-5/6 bg-white"></div>
+                <div class="h-1 w-5/6 bg-white"></div>
+                <div class="h-1 w-5/6 bg-white"></div>
+            </div>
+        </div>
+</template>
+
+<style scoped>
+    .container{
+        display: grid;
+        gap: 0.2rem;
+        grid-template-columns: 5rem auto 5rem;
+    }
+</style>
