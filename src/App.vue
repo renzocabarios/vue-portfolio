b1d12ad7640c971ba5b4d85ae27b6b16fe76657e
Renzo Cabarios <renzo.cabarios@gmail.com>
2022-08-29 16:39:00 +0800

    Add Card component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
 import NavBar from './components/NavBar.vue';
+import Card from './components/Card.vue';
 </script>
 
 <template>
         <NavBar/>
+        <div class="bg-gray-800 py-10 px-5">
+            <Card/>
+        </div>
 </template>
 
 <style scoped>
---
+++ src/components/Card.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="bg-gray-900 grid grid-rows-3 h-56">
+        <h1 class="row-span-2 bg-white"></h1>
+        <div class="flex justify-center items-center"><h1 class="text-center">TITLE</h1></div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
