2b6a6b982ee6c3cbeb89462be433edcd40023f1a
Renzo Cabarios <renzo.cabarios@gmail.com>
2022-09-14 01:07:40 +0800

    Update NavBar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,19 +2,24 @@
 </script>
 
 <template>
-        <div class="container bg-gray-900">
-            <div class="flex flex-col justify-evenly items-center px-3 py-1">
-                <div class="h-1 w-5/6 bg-white"></div>
-                <div class="h-1 w-5/6 bg-white"></div>
-                <div class="h-1 w-5/6 bg-white"></div>
-            </div>
+    <div class="nav bg-teal-400 text-white px-10">
+        <div class="flex justify-center items-center w-full h-full">
+            <h1 class="text-6xl font-black">Portolio</h1>
         </div>
+        <div></div>
+        <div class="flex flex-col justify-around w-full h-full p-2">
+            <div class="h-1/6 w-full bg-white rounded"></div>
+            <div class="h-1/6 w-full bg-white rounded"></div>
+            <div class="h-1/6 w-full bg-white rounded"></div>
+        </div>
+    </div>
 </template>
 
 <style scoped>
-    .container{
-        display: grid;
-        gap: 0.2rem;
-        grid-template-columns: 5rem auto 5rem;
-    }
+.nav {
+    width: 100%;
+    height: 100%;
+    display: grid;
+    grid-template-columns: max-content auto 5rem;
+}
 </style>
