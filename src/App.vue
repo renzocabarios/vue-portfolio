c667613d5af410f6dcb3e23243786dabe0742d94
Renzo Cabarios <renzo.cabarios@gmail.com>
2022-09-26 18:13:10 +0800

    Update ui

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
 <script setup lang="ts">
-import NavBar from './components/NavBar.vue';
-import Card from './components/Card.vue';
-import IntroView from './components/IntroView.vue';
-import FooterBar from './components/FooterBar.vue';
+
 
 
 </script>
@@ -15,76 +12,83 @@ export default {
         }
     },
     methods: {
-        student() {
-            window.location.href = "https://github.com/renzocabarios/laravue-student-crud";
+        qr_code() {
+            window.location.href = "https://github.com/renzocabarios/vanilla-web/tree/master/qr-code-component-main";
         },
-        pet_clinic() {
-            window.location.href = "https://github.com/renzocabarios/pet-clinic-mern";
+        preview() {
+            window.location.href = "https://github.com/renzocabarios/vanilla-web/tree/master/preview-card-component";
         },
-        user() {
-            window.location.href = "https://github.com/renzocabarios/mern-crud";
+        order_summary() {
+            window.location.href = "https://github.com/renzocabarios/vanilla-web/tree/master/order-summary-component-main";
         },
     }
 };
 </script>
 
 <template>
-    <IntroView />
-    <div class="project min-h-screen px-10">
-        <div class="col-span-2 justify-self-center">
-            <h1 class="text-6xl font-black text-teal-500">My Projects</h1>
+    <nav class=" bg-slate-400 w-full flex justify-between items-center h-20 px-10">
+        <p class="text-5xl font-black text-white">Portfolio</p>
+        <div class="text-white flex justify-center gap-5">
+            <span class="material-symbols-outlined">
+                home
+            </span>
+
+            <span class="material-symbols-outlined">
+                apps
+            </span>
         </div>
-        <div @click="user" class="bg-teal-500 rounded-lg shadow-lg hover:shadow-2xl transition-all">
-            <div class="w-full h-full flex justify-center items-center text-center">
-                <h1 class="text-5xl font-black text-white">User Management System</h1>
+    </nav>
+    <div class="min-h-screen px-10 grid grid-cols-2 text-black">
+        <div class="flex flex-col justify-evenly gap-1 px-24">
+            <div class="text-slate-400 font-black">
+                <p class="text-7xl">Renzo</p>
+                <p class="text-7xl">Cabarios</p>
             </div>
+            <p class="text-4xl text-slate-400">
+                I am a developer
+                who might not be the best, but I am capable enough to finish a job.
+            </p>
+            <button class="bg-slate-400 font-black text-white p-6 rounded shadow text-3xl">Let's Connect</button>
         </div>
-        <div @click="student" class="bg-teal-500 rounded-lg shadow-lg hover:shadow-2xl transition-all">
-            <div class="w-full h-full flex justify-center items-center text-center">
-                <h1 class="text-5xl font-black text-white">Student Management System</h1>
+        <div class="image"></div>
+    </div>
+    <div class="px-10 flex flex-col gap-10 ">
+        <p class="text-7xl text-slate-400 font-black text-center">My Projects</p>
+        <div class="flex flex-wrap justify-center w-full gap-10">
+            <div class="relative bg-slate-400 w-96 h-56 rounded shadow">
+                <span class="material-symbols-outlined text-white cursor-pointer" @click="preview">
+                    open_in_new
+                </span>
+                <h1 class="text-2xl absolute bottom-3 left-3 text-white">Preview Card Component</h1>
             </div>
-        </div>
-        <div @click="pet_clinic" class="bg-teal-500 rounded-lg shadow-lg hover:shadow-2xl transition-all">
-            <div class="w-full h-full flex justify-center items-center text-center">
-                <h1 class="text-5xl font-black text-white">Pet Clinic System</h1>
+            <div class="relative bg-slate-400 w-96 h-56 rounded shadow">
+                <span class="material-symbols-outlined text-white cursor-pointer" @click="qr_code">
+                    open_in_new
+                </span>
+                <h1 class="text-2xl absolute bottom-3 left-3 text-white">QR Code Component</h1>
+            </div>
+            <div class="relative bg-slate-400 w-96 h-56 rounded shadow">
+                <span class="material-symbols-outlined text-white cursor-pointer" @click="order_summary">
+                    open_in_new
+                </span>
+                <h1 class="text-2xl absolute bottom-3 left-3 text-white">Order Summary Component</h1>
             </div>
         </div>
     </div>
+
+    <div class="bg-slate-400 w-full p-12 mt-12"></div>
 </template>
 
+
 <style scoped>
-.intro-container {
-    width: 100%;
-    display: grid;
-    grid-template-rows: 9% auto;
-    gap: 2rem;
-
+.image {
+    background-image: url("./assets/img.png");
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-size: 100% 100%;
 }
 
-.intro {
-    width: 100%;
-    display: grid;
-    gap: 1rem;
-}
-
-.intro-img {
-    display: none;
-}
-
-@media only screen and (min-width: 600px) {
-    .intro {
-        grid-template-columns: 40% 60%;
-    }
-
-    .intro-img {
-        display: inline;
-    }
-
-    .project {
-        display: grid;
-        grid-template-rows: max-content auto;
-        gap: 3rem;
-        margin: 1rem 0;
-    }
+span {
+    font-size: 3rem;
 }
 </style>
