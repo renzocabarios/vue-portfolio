bc03dcbf06b3a0ee6568f6a987e09ce3266d1a92
Renzo Cabarios <renzo.cabarios@gmail.com>
2022-09-26 22:03:58 +0800

    Add MyProject

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script lang="ts">
 import NavBar from './utility/NavBar.vue';
 import Hero from './utility/Hero.vue';
+import MyProject from './utility/MyProject.vue';
 
 export default {
     setup() {
@@ -19,7 +20,7 @@ export default {
 
         return { qr_code, preview, order_summary }
     },
-    components: { NavBar, Hero }
+    components: { NavBar, Hero, MyProject }
 
 };
 </script>
@@ -27,44 +28,11 @@ export default {
 <template>
     <NavBar />
     <Hero />
-
-    <div class="px-10 flex flex-col gap-10 ">
-        <p class="text-7xl text-slate-400 font-black text-center">My Projects</p>
-        <div class="flex flex-wrap justify-center w-full gap-10">
-            <div class="relative bg-slate-400 w-1/4 h-56 rounded shadow">
-                <span class="material-symbols-outlined text-white cursor-pointer" @click="preview">
-                    open_in_new
-                </span>
-                <h1 class="text-2xl absolute bottom-3 left-3 text-white">Preview Card Component</h1>
-            </div>
-            <div class="relative bg-slate-400 w-1/4 h-56 rounded shadow">
-                <span class="material-symbols-outlined text-white cursor-pointer" @click="qr_code">
-                    open_in_new
-                </span>
-                <h1 class="text-2xl absolute bottom-3 left-3 text-white">QR Code Component</h1>
-            </div>
-            <div class="relative bg-slate-400 w-1/4 h-56 rounded shadow">
-                <span class="material-symbols-outlined text-white cursor-pointer" @click="order_summary">
-                    open_in_new
-                </span>
-                <h1 class="text-2xl absolute bottom-3 left-3 text-white">Order Summary Component</h1>
-            </div>
-        </div>
-    </div>
-
+    <MyProject />
     <div class="bg-slate-400 w-full p-12 mt-12"></div>
 </template>
 
 
 <style scoped>
-.image {
-    background-image: url("./assets/img.png");
-    background-repeat: no-repeat;
-    background-size: cover;
-    background-size: 100% 100%;
-}
 
-span {
-    font-size: 3rem;
-}
 </style>
---
+++ src/utility/MyProject.vue
@@ -0,0 +1,38 @@
+<script lang="ts">
+export default {
+    setup() { }
+};
+</script>
+
+<template>
+    <div class="px-10 flex flex-col gap-10 ">
+        <p class="text-7xl text-slate-400 font-black text-center">My Projects</p>
+        <div class="flex flex-wrap justify-center w-full gap-10">
+            <div class="relative bg-slate-400 w-1/4 h-56 rounded shadow">
+                <span class="material-symbols-outlined text-white cursor-pointer" @click="preview">
+                    open_in_new
+                </span>
+                <h1 class="text-2xl absolute bottom-3 left-3 text-white">Preview Card Component</h1>
+            </div>
+            <div class="relative bg-slate-400 w-1/4 h-56 rounded shadow">
+                <span class="material-symbols-outlined text-white cursor-pointer" @click="qr_code">
+                    open_in_new
+                </span>
+                <h1 class="text-2xl absolute bottom-3 left-3 text-white">QR Code Component</h1>
+            </div>
+            <div class="relative bg-slate-400 w-1/4 h-56 rounded shadow">
+                <span class="material-symbols-outlined text-white cursor-pointer" @click="order_summary">
+                    open_in_new
+                </span>
+                <h1 class="text-2xl absolute bottom-3 left-3 text-white">Order Summary Component</h1>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<style scoped>
+span {
+    font-size: 3rem;
+}
+</style>
